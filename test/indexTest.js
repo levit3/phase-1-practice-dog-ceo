0add1298d12c1f7232f071baad26751af8bb5967
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-12 11:40:52 +0300

    fetches data from api and adds event listeners

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,55 @@
-// The code below ensures that students who are using CodeGrade will get credit
-// for the code-along in Canvas; you can disregard it.
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const container = document.getElementById("dog-image-container");
+const breedList = document.getElementById("dog-breeds");
 
-require("./helpers.js");
+fetch(`${imgUrl}`)
+  .then((res) => res.json())
+  .then((data) => renderImage(data.message));
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
+function renderImage(data) {
+  data.forEach((image) => {
+    const img = document.createElement("img");
+    img.src = image;
+    container.appendChild(img);
+  });
+}
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+fetch(breedUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    renderBreed(data.message);
+    filter(data.message);
+  });
+
+function renderBreed(data) {
+  const breeds = Object.keys(data);
+  breeds.forEach((breed) => {
+    const list = document.createElement("li");
+    list.innerText = breed;
+    breedList.appendChild(list);
+    list.addEventListener("click", (e) => {
+      e.target.style.color = "red";
     });
   });
+}
+
+const option = document.getElementById("breed-dropdown");
+option.addEventListener("change", (event) => {
+  breedList.innerHTML = "";
+  fetch(breedUrl)
+    .then((res) => res.json())
+    .then((data) => filter(data.message, event));
 });
+
+function filter(data, event) {
+  const selectedLetter = event.target.value;
+  breedList.innerHTML = "";
+  const breeds = Object.keys(data);
+  breeds.forEach((breed) => {
+    if (breed.startsWith(selectedLetter)) {
+      const listItem = document.createElement("li");
+      listItem.innerText = breed;
+      breedList.appendChild(listItem);
+    }
+  });
+}
